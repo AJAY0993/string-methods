4fe121dd10d1fb7385265c556682bb99f2989f63
AJAY0993 <128742355+AJAY0993@users.noreply.github.com>
2023-08-02 21:24:18 +0530

    Create string-methods.js

---
+++ string-methods.js
@@ -0,0 +1,54 @@
+let sentence = "real eyes realize real lies"
+let index = 8
+console.log(sentence.at(index))
+
+console.log("marathon".at(7))
+console.log("rahul".charAt(2))
+console.log("rahul".at(2))
+
+console.log("vikram".endsWith("m"))
+console.log("vikram".endsWith("j"))
+
+sentence = "something somewhat somewhere sometime"
+
+console.log(sentence.endsWith("ping"))
+console.log(sentence.endsWith("sometime"))
+console.log(sentence.endsWith("sometime"))
+console.log(sentence.endsWith("e"))
+console.log(sentence.startsWith("somewhere"))
+console.log(sentence.startsWith("something"))
+
+
+let string1 = "bob"
+
+console.log(string1.concat(" ","bobber"))
+console.log("string1") // so it does return new string
+
+string1 = "  jai ho   "
+console.log(string1)
+console.log(string1.trim())
+
+console.log(string1)
+let string2 = string1.trim()
+console.log(string2)
+
+console.log(string1.trimEnd())
+console.log(string1.trimStart())
+console.log(string1.length)
+
+console.log(string1.trimEnd().length)
+console.log(string1.trimStart().length)
+
+
+let stringObj =new String("bla bla")
+
+
+// console.log(new String("bla bla"))
+
+console.log(stringObj.valueOf())
+
+let stringObj2 = new String("gka lgla") 
+
+console.log(stringObj2.valueOf())
+
+
